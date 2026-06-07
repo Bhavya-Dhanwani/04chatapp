c0a1b35bddd3b5cb24e4043eb83f04a774822496
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-06-07 18:28:15 +0530

    Cors Solved

--- backend/src/middlewares/cors.middleware.js
+++ backend/src/middlewares/cors.middleware.js
@@ -1,6 +1,6 @@
 // CORS middleware
 function corsMiddleware(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
